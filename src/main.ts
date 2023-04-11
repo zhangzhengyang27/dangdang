4dca84feb01a742a42c5f1de96d86c261767b2c6
zhangzhengyang <zhangzhengyang27@aliyun.com>
2023-04-11 11:32:25 +0800

    环境变量配置

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+console.log('环境变量:', import.meta.env.VITE_username)
+console.log('环境变量:', import.meta.env.VITE_age)
+
 createApp(App).mount('#app')
